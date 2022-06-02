1e8b63486e18a35f107491daa36d54cf0e1a263e
Adriano Grimolizzi <adriano.grimolizzi@symphony.com>
2022-06-02 09:37:14 +0200

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from "express"
 
-const app = express();
+const app = express()
 
 app.get("/hello", (_req, res) => {
   res.send("hello world");
-});
+})
 
 app.listen(3000, () => {
   console.log("Server is Running!");
-});
+})
